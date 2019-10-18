25834b94adc724b5737861e959cb521c8987b5c0
Doug Friedel <friedel@illinois.edu>
2019-10-18 12:02:33 -0500

    pylint update

--- python/despydb/desdbi_defs.py
+++ python/despydb/desdbi_defs.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
 # $Id: desdbi_defs.py 33097 2015-01-20 15:14:29Z felipe $
 # $Rev:: 33097                            $:  # Revision of last commit.
 # $LastChangedBy:: felipe                 $:  # Author of last commit.
 # $LastChangedDate:: 2015-01-20 09:14:29 #$:  # Date of last commit.
-
+"""
+    Module constants for using cx_Oracle
+"""
 # cx_Oracle indexes for cursor.description sequences
 COL_NAME = 0
 COL_TYPE = 1
@@ -12,4 +13,3 @@ COL_LENGTH = 3
 COL_PRECISION = 4
 COL_SCALE = 5
 COL_NULLOK = 6
-
